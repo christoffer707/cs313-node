76927d92a5e0e5f3897d484847e05a46b65fa04e
christoffer707 <Christoffer707@gmail.com>
2018-03-10 22:17:18 -0800

    added project

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs'); 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.get('/', function (request, response) {
+	response.render('pages/index')
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+});
+  app.get('/postal', function (request, response) {
+	response.render('pages/postalCal')
+
+});
+
+app.post('/getRate', function (req, res){	
+	var type = req.body.type;
+	var weights = req.body.weights;
+	res.render('pages/getRate', {weights:weights, type:type});
+});
+
+app.listen(app.get('port'), function() {
+console.log('Node app is running on port', app.get('port'));
+});
+
+
+
+
